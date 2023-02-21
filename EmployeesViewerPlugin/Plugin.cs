b0edc0ba448aac753272edbd88a9f1f1bb6a8bfd
thefairman <61229964+thefairman@users.noreply.github.com>
2023-02-21 06:56:57 -0500

    fix del command

--- EmployeesViewerPlugin/Plugin.cs
+++ EmployeesViewerPlugin/Plugin.cs
@@ -57,7 +57,7 @@ namespace EmployeesLoaderPlugin
             {
               logger.Error("Not an index or not an int value!");
             } else {
-              if(indexToDelete > 0 && indexToDelete < employeesList.Count())
+              if(indexToDelete >= 0 && indexToDelete < employeesList.Count())
               {
                 employeesList.RemoveAt(indexToDelete);
               }
